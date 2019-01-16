39c6d744185d04d15a6cc9705348ecd7145c5c73
Nate Rudnick-Aldridge <nate@cratebind.com>
2019-01-15 19:36:56 -0600

    more printer additions

--- printer-side/print.py
+++ printer-side/print.py
@@ -1,6 +1,9 @@
 import requests
 import schedule
 import time
+import re
+import cStringIO
+from PIL import Image
 
 def print_messages():
     print("hello")
@@ -10,6 +13,14 @@ def print_messages():
         print(message["id"])
         print(message["text"])
         print(message["image"])
+
+        image_data = re.sub('^data:image/.+;base64,', '', message["image"]).decode('base64')
+        image = Image.open(cStringIO.StringIO(image_data))
+        image.save("test_drawing.png")
+        # image = image.convert("L")
+        # image.save("test_drawing_gray.png")
+        image = image.convert("1")
+        image.save("test_drawing_1bit.bmp")
         requests.delete("http://localhost:3001/messages/%s" %(id))
 
 schedule.every(5).seconds.do(print_messages)
