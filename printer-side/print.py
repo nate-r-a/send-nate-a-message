ac0f182413dba3ce3c427ea21076af619bb3d7cf
Nate <nr265@cornell.edu>
2019-02-06 05:21:44 +0000

    So many printer things

--- printer-side/print.py
+++ printer-side/print.py
@@ -2,18 +2,25 @@ import requests
 import schedule
 import time
 import re
+import os
 import subprocess
 import cStringIO
-from PIL import Image
+import printer
+from PIL import Image, ImageDraw
 
 # TODO: Edit zj-58 driver to make the media size smaller?
 
 subprocess.call(["stty", "-F", "/dev/ttyUSB0", "19200"])
+#subprocess.call(["stty", "-F", "/dev/ttyAMA1", "19200"])
+
+p = printer.ThermalPrinter()
+#p = printer.ThermalPrinter(serialport="/dev/ttyAMA1")
+
 
 def print_messages():
     print("hello")
     # r = requests.get("http://localhost:3001/messages")
-    r = requests.get("https://polar-mountain-80188.herokuapp.com/messages")
+    r = requests.get("https://send-nate-a-message.herokuapp.com/messages")
     for message in r.json():
         id = message["id"]
         print(message["id"])
@@ -24,21 +31,34 @@ def print_messages():
 
         image = Image.open(cStringIO.StringIO(image_data))
         image.save("drawing.png")
+        # image.save("pictures/%s.png" %(int(round(time.time() * 1000))))
 
-        image = image.convert("L")
-        image.save("drawing_gray.png")
+        # image = image.convert("L")
+        # image.save("drawing_gray.png")
 
-        image = image.convert("1")
-        image.save("drawing_1bit.bmp")
+        # image = image.convert("1")
+        # image.save("drawing_1bit.bmp")
 
-        # subprocess.call(["lp", "-o fit-to-page", "drawing_1bit.bmp"])
-        # subprocess.call(["echo", "-e", message["text"], ">", "/dev/ttyUSB0" ])
+        # image.save('drawing.pdf', 'pdf')
 
+        if message["text"]:
+            p.print_text(message["text"])
+            p.print_text("\n")
+            #os.system("echo %s > /dev/ttyUSB0" %(message["text"]))
+
+        # i = Image.open("drawing.png")
+        data = list(image.getdata())
+        w, h = image.size
+        p.print_bitmap_slow(data, w, h, True)
+        #subprocess.call(["lp", "-o fit-to-page", "drawing.pdf"])
+        # subprocess.call(["lp", "drawing.png"])
+
+        requests.put("https://send-nate-a-message.herokuapp.com/messages/%s" %(id), params={ 'printed': True })
         # requests.delete("http://localhost:3001/messages/%s" %(id))
-        requests.delete("https://polar-mountain-80188.herokuapp.com/messages/%s" %(id))
+        # requests.delete("https://send-nate-a-message.herokuapp.com/messages/%s" %(id))
 
 print_messages()
-schedule.every(5).seconds.do(print_messages)
+schedule.every(150).seconds.do(print_messages)
 
 while True:
     schedule.run_pending()
